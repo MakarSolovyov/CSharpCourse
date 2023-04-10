99b6b30f208ebcc1c56b23253d8870a238d9028b
Makar Solovyov <solovyovmakar@gmail.com>
2023-04-11 00:09:12 +0700

    Добавление решения для работы

---
+++ LabWork4/Laba4/Model/MotionFactoryBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// Base class for motion factory.
+    /// </summary>
+    public abstract class MotionFactoryBase
+    {
+        /// <summary>
+        /// Get instance of a certain motion.
+        /// </summary>
+        /// <returns>An instance of a motion.</returns>
+        public abstract MotionBase GetInstance(MotionType motionType);
+        
+        /// <summary>
+        /// Get random value.
+        /// </summary>
+        /// <param name="maxValue">Max value.</param>
+        /// <param name="onlyPositive">Input True to get positive value
+        /// for sure.</param>
+        /// <returns>A positive/negative value.</returns>
+        public double GetRandomValue(int maxValue, bool onlyPositive)
+        {
+            var rnd = new Random();
+            var plusMinus = rnd.Next(2);
+            var tmpValue = plusMinus == 0
+                ? Math.Round(rnd.NextDouble() * maxValue, 2)
+                : -Math.Round(rnd.NextDouble() * maxValue, 2);
+
+            if (onlyPositive)
+            {
+                tmpValue = Math.Abs(tmpValue);
+            }
+
+            return tmpValue;
+        }
+    }
+}
